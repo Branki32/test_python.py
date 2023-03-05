68cf6619a1c4f01119abda3640daa5fe6b808976
Branki32 <126017982+Branki32@users.noreply.github.com>
2023-03-05 12:30:25 +0100

    Add files via upload

---
+++ test_python.py
@@ -0,0 +1,16 @@
+someNumber = 10
+
+if(someNumber > 0):
+    print("Over zerro")
+elif(someNumber < 0):
+    print("Bellow zerro")
+else:
+    print("zerro")
+
+    H = 10 
+a = 1 
+for i in range(H): 
+ for j in range(a): 
+ print("*",end="") 
+ a+=1 
+ print("")
